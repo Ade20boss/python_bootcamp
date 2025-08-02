db344e9ca1fff78ea98d5c25bc4ed132f8dd3ebe
Ade20boss <danielademoye3@gmail.com>
2025-08-02 05:30:22 -0700

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,5 @@
+import math
+radius = float(input("Enter radius of circle: "))
+area = (math.pi) * (radius**2)
+circumference = 2 * (math.pi) * radius
+print(f"The area of the circle is {area} and the circumference is {circumference}")
