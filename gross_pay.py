d86c3c10502b012731cd64f466c75ddb9de5d9a7
Ade20boss <danielademoye3@gmail.com>
2025-08-02 16:12:43 -0700

    Update gross_pay.py

--- gross_pay.py
+++ gross_pay.py
@@ -1,3 +1,16 @@
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate: "))
-print(f"Pay: {round(rate * hours, 2)}")
+pay = 0
+if hours <= 40:
+	pay = round(hours * rate, 2)
+	print(f"Your pay is {pay}")
+elif hours > 40:
+	overtime = hours - 40
+	overtime_pay = (rate * 1.5) * overtime
+	pay = round((hours - overtime) * rate, 2) + overtime_pay
+	print(f"You worked for more than 40 hours, your payment plus overtime is: {pay}")
+	
+
+
+
+
