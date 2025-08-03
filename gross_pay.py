120402b7747fd6c85ff53f61d6eb5c2108558872
Ade20boss <danielademoye3@gmail.com>
2025-08-02 21:36:13 -0700

    Update gross_pay.py

--- gross_pay.py
+++ gross_pay.py
@@ -1,5 +1,16 @@
-hours = float(input("Enter Hours: "))
-rate = float(input("Enter Rate: "))
+hour = input("Enter Hours: ")
+try:
+	hours = float(input("Enter Hours: "))
+except ValueError:
+	print("Error, please enter numeric input for hour")
+	quit()
+	
+try:
+	rate = float(input("Enter Rate: "))
+except ValueError:
+	print("Error, please enter numeric input for hour")
+	quit()
+	
 pay = 0
 if hours <= 40:
 	pay = round(hours * rate, 2)
@@ -10,6 +21,7 @@ elif hours > 40:
 	pay = round((hours - overtime) * rate, 2) + overtime_pay
 	print(f"You worked for more than 40 hours, your payment plus overtime is: {pay}")
 	
+
 
 
 
